fc76a11afa266d195e479be6a3b0dbced2338e64
Marbevi <marfran2022@gmail.com>
2023-07-12 19:58:05 -0300

    Altera nome da classe para Aplication Biblioteca

--- src/main/java/com/teste/implementabiblioteca/Aplication_Biblioteca.java
+++ src/main/java/com/teste/implementabiblioteca/Aplication_Biblioteca.java
@@ -2,13 +2,12 @@ package com.teste.implementabiblioteca;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 
-public class Controller_Aplication_Biblioteca {
+public class Aplication_Biblioteca {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Controller_Aplication_Biblioteca.class, args);
-    }
+    public static void main(String[] args) { SpringApplication.run(Aplication_Biblioteca.class, args); }
 
 
 }
