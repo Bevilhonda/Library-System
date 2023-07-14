51c42e5bc2b7462f6a6d7264ffbe750dbc5d2cea
Bevilhonda <marcelinyuri@gmail.com>
2023-07-14 10:39:36 -0300

    testa a classe Biblioteca

--- src/test/java/BibliotecaTest.java
+++ src/test/java/BibliotecaTest.java
@@ -2,6 +2,7 @@ import com.teste.implementabiblioteca.Biblioteca;
 import com.teste.implementabiblioteca.Endereco;
 import com.teste.implementabiblioteca.Livro;
 import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,10 +19,10 @@ class BibliotecaTest {
                 ("Afonso", 25, "Centro", "Maringá", "Paraná", 1);
 
         Biblioteca nova_biblioteca = new Biblioteca("Sarandi", livro_atual, novo_endereco, 1);
-        assertEquals("Sarandi",nova_biblioteca.getNome_da_biblioteca());
-        assertEquals(livro_atual,nova_biblioteca.getLista_de_livro());
-        assertEquals(novo_endereco,nova_biblioteca.getEndereco());
-        assertEquals(1,nova_biblioteca.getId_biblioteca());
+        assertEquals("Sarandi", nova_biblioteca.getNome_da_biblioteca());
+        assertEquals(livro_atual, nova_biblioteca.getLista_de_livro());
+        assertEquals(novo_endereco, nova_biblioteca.getEndereco());
+        assertEquals(1, nova_biblioteca.getId_biblioteca());
 
     }
 
