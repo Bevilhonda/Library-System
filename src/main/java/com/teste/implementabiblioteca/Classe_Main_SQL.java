759ad44251e68735d78e222b0ee5c1a96c14c1f8
Bevilhonda <marcelinyuri@gmail.com>
2023-07-14 11:28:03 -0300

    treinamento de construção de classes e funções

--- src/main/java/com/teste/implementabiblioteca/Classe_Main_SQL.java
+++ src/main/java/com/teste/implementabiblioteca/Classe_Main_SQL.java
@@ -1,9 +1,5 @@
 package com.teste.implementabiblioteca;
 
-import com.teste.implementabiblioteca.Autor;
-import com.teste.implementabiblioteca.Funcoes_SQL;
-import com.teste.implementabiblioteca.Livro;
-
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
@@ -12,22 +8,14 @@ public class Classe_Main_SQL {
 
     public static void main(String[] args) {
         Instant data_nascimento_autor1 = LocalDateTime.parse("1975-03-10t20:30:00").toInstant(ZoneOffset.UTC);
-        Instant data_nascimento_autor2 = LocalDateTime.parse("1960-10-10t20:30:00").toInstant(ZoneOffset.UTC);
-        Instant data_nascimento_autor3 = LocalDateTime.parse("1950-02-10t20:30:00").toInstant(ZoneOffset.UTC);
-        Instant data_nascimento_autor4 = LocalDateTime.parse("1930-12-30t20:30:00").toInstant(ZoneOffset.UTC);
         Instant data_nascimento_autor5 = LocalDateTime.parse("1965-01-10t20:30:00").toInstant(ZoneOffset.UTC);
 
         Autor autor = new Autor("Mendes", "Sá", data_nascimento_autor1, 251);
-        Autor autor1 = new Autor("Fred", "Smith", data_nascimento_autor5, 239);
-        Autor autor2 = new Autor("Teodoro", "March", data_nascimento_autor5, 249);
-        Autor autor3 = new Autor("Marina", "Silva", data_nascimento_autor5, 240);
+        Autor autor1 = new Autor("Fred", "Smith", data_nascimento_autor5, 272);
 
         Instant data_publicacao = LocalDateTime.parse("1990-05-25t20:30:00").toInstant(ZoneOffset.UTC);
 
-        Livro livro = new Livro("Mundo java novo", autor1, data_publicacao, 1, 1, 519);
-        Livro livro1 = new Livro("Banco  2023", autor1, data_publicacao, 1, 1, 513);
-        Livro livro2 = new Livro(" Android 2023", autor, data_publicacao, 1, 1, 514);
-        Livro livro3 = new Livro(" Android New", autor, data_publicacao, 1, 1, 528);
+        Livro livro = new Livro("Mundo java novo", autor1, data_publicacao, 1, 1, 531);
 
         //livro.Print_Livro();// aqui imprime os livros cadastrados no banco
 
@@ -38,38 +26,39 @@ public class Classe_Main_SQL {
 
         //XXXX Aqui insere Livros no banco de dados mysql.-- 100 testado
 
-        //insert.Inserir_Livros(livro);
+        //insert.Comando_Insert(livro);
 
         //XXXXX Aqui atualiza dados do com.teste.implementabiblioteca.Livro no banco mysql. -- 100%testado
 
-        //insert.Atualiza_Livros(livro);
+        //insert.Comando_Update(livro);
 
         //XXXXX Aqui exclui livros no banco de dados mysql -- 100% testado
 
-        //insert.Exclui_Livro(521);
+        //insert.Comando_Delete(521);
 
         // XXXXX Aqui seleciona um livro no banco ao escolher um numero de ID -- 100% testado
-        // insert.Seleciona_um_Livro();
+
+        // insert.Comando_Select_Por_Id();
 
         //XXXXX Aqui seleciona todos os livros de um autor ao escolher o ID do autor -- 100% testado
 
-        // insert.Seleciona_TodosLivros_Autor();
+        // insert.Comando_Select_Autores();
 
         //XXXXX Aqui insere um autor no Banco mysql -- 100% testado
 
-        // insert.Incluir_Autor(autor);
+        // insert.Comando_Insert_Autor(autor);
 
         //XXXX Aqui atualizada dados do com.teste.implementabiblioteca.Autor no Banco mysql -- 100% testado
 
-        // insert.Update_Autor(autor2);
+        // insert.Comando_Update_Autor(autor2);
 
-        Input_Dados_Autor input_autor = new Input_Dados_Autor();//CADASTRA UM AUTOR NO BANCO -- 100% testado
+        Inserir_Autor_No_Banco input_autor = new Inserir_Autor_No_Banco();//CADASTRA UM AUTOR NO BANCO -- 100% testado
         //input_autor.Inserir_Autor();
 
-        Input_Dados_Endereco novo_endereco = new Input_Dados_Endereco();// CADASTRA UM ENDEREÇO NO BANCO -- 100% testado
+        Inserir_Endereco_No_Banco novo_endereco = new Inserir_Endereco_No_Banco();// CADASTRA UM ENDEREÇO NO BANCO -- 100% testado
         //novo_endereco.Inserir_Dados_Endereco();
 
-        Pesquisa_LivroBanco mostra_livro = new Pesquisa_LivroBanco();// PESQUISA LIVRO NO BANCO -- 100% testado
+        Seleciona_Dados_Livro_No_Banco mostra_livro = new Seleciona_Dados_Livro_No_Banco();// PESQUISA LIVRO NO BANCO -- 100% testado
         //mostra_livro.Seleciona_LivroBanco();
 
     }
