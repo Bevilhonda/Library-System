ba7a48875bfd31b5e0c686af812c0743008f2714
Bevilhonda <marcelinyuri@gmail.com>
2023-07-14 10:34:33 -0300

    estende outra classe para criar exceções especificas

--- src/main/java/com/teste/implementabiblioteca/AutorNotFound.java
+++ src/main/java/com/teste/implementabiblioteca/AutorNotFound.java
@@ -1,5 +1,24 @@
 package com.teste.implementabiblioteca;
 
 public class AutorNotFound extends ResponseTypeExceptions {
+    private Integer id ;
+    private String sobrenome;
+    public AutorNotFound() {
 
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
