51dcec729853beab6d9c7436676975c20ced7c77
Bevilhonda <marcelinyuri@gmail.com>
2023-07-14 13:16:18 -0300

    classe Endereco usando anotações do spring boot

---
+++ src/main/java/com/teste/implementabiblioteca/Model/Create_Endereco.java
@@ -0,0 +1,20 @@
+package com.teste.implementabiblioteca.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+
+public class Create_Endereco {
+    @Id
+    @GeneratedValue
+
+    String rua;
+    Integer numero;
+    String bairro;
+    String cidade;
+    String estado;
+}
