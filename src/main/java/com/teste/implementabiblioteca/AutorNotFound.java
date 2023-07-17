264ad2e17541e647adda1c729d7a399ea9a32fa8
Bevilhonda <marcelinyuri@gmail.com>
2023-07-17 20:54:20 -0300

    remove metodo set

--- src/main/java/com/teste/implementabiblioteca/AutorNotFound.java
+++ src/main/java/com/teste/implementabiblioteca/AutorNotFound.java
@@ -1,24 +1,12 @@
 package com.teste.implementabiblioteca;
 
 public class AutorNotFound extends ResponseTypeExceptions {
-    private Integer id ;
-    private String sobrenome;
-    public AutorNotFound() {
-
-    }
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
+    private Integer id;
+    public AutorNotFound(Integer id) {
+        this.id = id;
     }
 
     public Integer getId() {
         return id;
-    }
-    public void setId(Integer id) {
-        this.id = id;
     }
 }
