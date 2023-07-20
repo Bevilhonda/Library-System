ef40b8da8b2cb2ddaeadcad85a36e2706ccf5dd7
Bevilhonda <marcelinyuri@gmail.com>
2023-07-20 08:35:36 -0300

    altera nome da classe

--- src/main/java/com/teste/implementabiblioteca/Model/BibliotecaEntity.java
+++ src/main/java/com/teste/implementabiblioteca/Model/BibliotecaEntity.java
@@ -4,18 +4,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.time.Instant;
 
 @Entity
 @Table
 
-public class Create_Livro {
+public class BibliotecaEntity {
+
     @Id
     @GeneratedValue
 
-    String titulo;
-    Integer edicao;
-    Integer fk_autor;
-    Integer fk_biblioteca;
-    Instant data_publication;
+    String nome_da_biblioteca;
+    Integer fk_endereco;
 }
--- src/main/java/com/teste/implementabiblioteca/Model/LivroEntity.java
+++ src/main/java/com/teste/implementabiblioteca/Model/LivroEntity.java
@@ -4,15 +4,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.time.Instant;
 
 @Entity
 @Table
 
-public class Create_Biblioteca {
-
+public class LivroEntity {
     @Id
     @GeneratedValue
 
-    String nome_da_biblioteca;
-    Integer fk_endereco;
+    String titulo;
+    Integer edicao;
+    Integer fk_autor;
+    Integer fk_biblioteca;
+    Instant data_publication;
 }
