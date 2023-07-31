37f385bbd6b1c689635a9ac3474a68a5f078f0cb
Bevilhonda <marcelinyuri@gmail.com>
2023-07-31 11:31:52 -0300

    cria classe RepositoryAddress

--- src/main/java/com/teste/implementabiblioteca/repository/RepositoryAddress.java
+++ src/main/java/com/teste/implementabiblioteca/repository/RepositoryAddress.java
@@ -2,6 +2,7 @@ package com.teste.implementabiblioteca.repository;
 
 import com.teste.implementabiblioteca.Model.AddressEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -15,4 +16,13 @@ public interface RepositoryAddress extends JpaRepository<AddressEntity, Integer>
 
     @Query(value = "select * from Endereco where id_endereco = :id_endereco ", nativeQuery = true)
     AddressEntity GetAddress(Integer id_endereco);
+
+    @Modifying
+    @Query(value = "Insert into Endereco( rua,numero,bairro,cidade,estado) values " +
+            "(:rua,:numero,:bairro,:cidade,:estado)",nativeQuery = true)
+    Integer Insert(String rua , Integer numero,String bairro, String cidade,String estado);
+    @Modifying
+    @Query(value = "UPDATE Endereco set rua = :rua , numero = :numero , bairro = :bairro ," +
+            " cidade = :cidade , estado = :estado  where id_endereco = :id_endereco", nativeQuery = true)
+    Integer UpdateAddress(String rua , Integer numero,String bairro, String cidade,String estado, Integer id_endereco);
 }
