0af4ba78324bfe910e45ef7672a307e38d56ec53
Bevilhonda <marcelinyuri@gmail.com>
2023-08-10 18:39:34 -0300

    organiza código

--- src/main/java/com/teste/implementabiblioteca/Controller/Author/FormatterResponse/ResponseFormatter.java
+++ src/main/java/com/teste/implementabiblioteca/Controller/Author/FormatterResponse/ResponseFormatter.java
@@ -5,7 +5,7 @@ import com.teste.implementabiblioteca.Model.AuthorEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-public class ResponseFormatterAuthor {
+public class ResponseFormatter {
 
     public static ResponseEntity<?> FormatAuthorResponse(AuthorEntity author) {
         HttpStatus status = HttpStatus.OK;
