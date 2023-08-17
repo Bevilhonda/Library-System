894f62f03ec8272a5cb1ce3ddad9154308e9de22
Bevilhonda <marcelinyuri@gmail.com>
2023-08-17 16:28:31 -0300

    renomeia classe

--- src/main/java/com/teste/implementabiblioteca/Services/Author/ClassServices/DoAllAuthors.java
+++ src/main/java/com/teste/implementabiblioteca/Services/Author/ClassServices/DoAllAuthors.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
-public class SearchAllAuthors {
+public class DoAllAuthors {
     @Autowired
     private RepositoryAuthor repositoryAuthor;
 
