91c532d9c1d49b04465b2cf1fad48bd572fd3111
Bevilhonda <marcelinyuri@gmail.com>
2023-08-28 12:54:33 -0300

    organiza funções e classes

--- src/main/java/com/teste/implementabiblioteca/Services/Library/ServicesLibrary.java
+++ src/main/java/com/teste/implementabiblioteca/Services/Library/ServicesLibrary.java
@@ -1,53 +1,36 @@
 package com.teste.implementabiblioteca.Services.Library;
 
-import com.teste.implementabiblioteca.Controller.Library.Exceptions.ErrorHandling.LibraryExceptions;
+
 import com.teste.implementabiblioteca.Controller.Library.Exceptions.TypeExceptions.LibraryNotFound;
 import com.teste.implementabiblioteca.Controller.Library.Exceptions.TypeExceptions.NameLibraryNotFound;
 import com.teste.implementabiblioteca.Model.LibraryEntity;
 import com.teste.implementabiblioteca.Repository.RepositoryLibrary;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
-import static com.teste.implementabiblioteca.Controller.Library.Exceptions.ErrorHandling.ErrorHandlingLibrary.MapLibrary;
-import static com.teste.implementabiblioteca.Controller.Library.FormatterResponse.HelperResponseLibrary.DetailsAllLibrary;
-import static com.teste.implementabiblioteca.Controller.Library.FormatterResponse.HelperResponseLibrary.DetailsLibrary;
-
 @Service
 public class ServicesLibrary {
     @Autowired
     private RepositoryLibrary repositorylibrary;
 
-    public ResponseEntity<?> GetLibraryById(Integer id) {
-        try {
+    public LibraryEntity GetLibraryById(Integer id) throws LibraryNotFound {
 
-            LibraryEntity library =repositorylibrary.GetLibrary(id);
+        LibraryEntity library = repositorylibrary.GetLibrary(id);
 
-            if (library == null) {
-                throw new LibraryNotFound(id);
-            }
-            return DetailsLibrary("id: " + library.getIdLibrary() + "\n Nome: " +
-                            library.getName() + "\n ID Endereço: " + library.getFkAddress(),
-                    HttpStatus.OK);
-
-        } catch (LibraryExceptions e) {
-            return MapLibrary(e);
+        if (library == null) {
+            throw new LibraryNotFound(id);
         }
+        return library;
     }
 
-    public ResponseEntity<?> GetLibraryByName(String nome) {
-        try {
+    public List<LibraryEntity> GetLibraryByName(String nome) throws NameLibraryNotFound {
+
             List<LibraryEntity> name = repositorylibrary.GetAllName(nome);
+
             if (name.isEmpty()) {
                 throw new NameLibraryNotFound(nome);
             }
-            return DetailsAllLibrary(name, HttpStatus.OK);
-        } catch (LibraryExceptions e) {
-            return MapLibrary(e);
+            return name;
         }
-    }
-
 }
