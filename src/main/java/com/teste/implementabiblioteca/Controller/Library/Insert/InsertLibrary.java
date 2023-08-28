80475d31c68f30ce68c9ef63e757b49d958687ac
Bevilhonda <marcelinyuri@gmail.com>
2023-08-28 12:55:33 -0300

    cria endpoints de library

---
+++ src/main/java/com/teste/implementabiblioteca/Controller/Library/Insert/InsertLibrary.java
@@ -0,0 +1,7 @@
+package com.teste.implementabiblioteca.Controller.Library.Insert;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InsertLibrary {
+}
---
+++ src/main/java/com/teste/implementabiblioteca/Controller/Library/Update/UpdateLibrary.java
@@ -0,0 +1,4 @@
+package com.teste.implementabiblioteca.Controller.Library.Update;
+
+public class UpdateLibrary {
+}
