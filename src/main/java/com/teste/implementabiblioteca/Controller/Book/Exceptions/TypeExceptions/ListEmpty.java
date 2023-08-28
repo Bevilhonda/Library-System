b76412e9dccfb787a81b54d913405ce3dfea2498
Bevilhonda <marcelinyuri@gmail.com>
2023-08-28 11:59:20 -0300

    cria e organiza funções de exceptions

---
+++ src/main/java/com/teste/implementabiblioteca/Controller/Book/Exceptions/TypeExceptions/ListEmpty.java
@@ -0,0 +1,6 @@
+package com.teste.implementabiblioteca.Controller.Book.Exceptions.TypeExceptions;
+
+import com.teste.implementabiblioteca.Controller.Book.Exceptions.ErrorHandling.BookExceptions;
+
+public class ListEmpty extends BookExceptions {
+}
