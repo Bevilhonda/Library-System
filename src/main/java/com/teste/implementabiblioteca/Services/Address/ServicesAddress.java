b92459fbd51f96cb8d72da5383b774e89d19f03d
Bevilhonda <marcelinyuri@gmail.com>
2023-08-28 11:56:09 -0300

    renomeia e organiza classes de Services

--- src/main/java/com/teste/implementabiblioteca/Services/Address/ServicesAddress.java
+++ src/main/java/com/teste/implementabiblioteca/Services/Address/ServicesAddress.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class Services {
+public class ServicesAddress {
     @Autowired
     private RepositoryAddress repository;
 
--- src/main/java/com/teste/implementabiblioteca/Services/Author/ServicesAuthor.java
+++ src/main/java/com/teste/implementabiblioteca/Services/Author/ServicesAuthor.java
@@ -14,7 +14,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
-public class Services {
+public class ServicesAuthor {
     @Autowired
     private RepositoryAuthor repository;
 
