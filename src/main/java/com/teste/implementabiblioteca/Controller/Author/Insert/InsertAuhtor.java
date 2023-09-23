8ed5d1c15abe89653a5981a267cf2ac06994fe7c
Bevilhonda <marcelinyuri@gmail.com>
2023-09-23 07:20:50 -0300

    inclui  anotação de validação

--- src/main/java/com/teste/implementabiblioteca/Controller/Author/Insert/InsertAuhtor.java
+++ src/main/java/com/teste/implementabiblioteca/Controller/Author/Insert/InsertAuhtor.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 import static com.teste.implementabiblioteca.Controller.Author.ExceptionHandler.Handler.map;
 import static org.springframework.http.HttpStatus.*;
 
@@ -17,7 +19,7 @@ public class InsertAuhtor {
     private ServicesAuthor service;
 
     @PostMapping("/InsertAuthor")
-    public ResponseEntity<?> insert(@RequestBody DataAuthorEntity author) {
+    public ResponseEntity<?> insert( @RequestBody @Valid DataAuthorEntity author) {
         try {
             service.insert(author.toModel());
 
