7e1c1ecb5ebaa49ccf9b17bb1a146c3d1dda5b7a
Bevilhonda <marcelinyuri@gmail.com>
2023-09-23 11:27:54 -0300

    atualiza anotações de validação

--- src/main/java/com/teste/implementabiblioteca/Controller/Address/Insert/InsertAddress.java
+++ src/main/java/com/teste/implementabiblioteca/Controller/Address/Insert/InsertAddress.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 import static com.teste.implementabiblioteca.Controller.Book.ExceptionHandler.Handler.map;
 import static org.springframework.http.HttpStatus.*;
 
@@ -18,7 +20,7 @@ public class InsertAddress {
     private ServicesAddress service;
 
     @PostMapping("/Insert/Address")
-    public ResponseEntity<?> insertAddress(@RequestBody DataAddressEntity newAddress) {
+    public ResponseEntity<?> insertAddress(@RequestBody @Valid DataAddressEntity newAddress) {
         try {
             service.insertAddress(newAddress.toModel());
 
--- src/main/java/com/teste/implementabiblioteca/Controller/Book/Insert/DTO/Insert.java
+++ src/main/java/com/teste/implementabiblioteca/Controller/Book/Insert/DTO/Insert.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 import static com.teste.implementabiblioteca.Controller.Book.ExceptionHandler.Handler.map;
 import static org.springframework.http.HttpStatus.*;
 
@@ -15,7 +17,7 @@ public class Insert {
     @Autowired
     private ServicesBook service;
     @PostMapping("/Insert/Book")
-    public ResponseEntity<?> insert(@RequestBody DataBookEntity book){
+    public ResponseEntity<?> insert(@RequestBody @Valid  DataBookEntity book){
         try {
              service.insert(book.toModel());
             return ResponseEntity.status(OK).build();
