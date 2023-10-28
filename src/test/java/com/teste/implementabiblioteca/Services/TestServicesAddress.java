dce9c7a2befef990bb0a0963ac45507425f83b86
Bevilhonda <marcelinyuri@gmail.com>
2023-10-27 21:45:57 -0300

    organiza classe

--- src/test/java/com/teste/implementabiblioteca/Services/TestServicesAddress.java
+++ src/test/java/com/teste/implementabiblioteca/Services/TestServicesAddress.java
@@ -16,8 +16,7 @@ import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatObject;
+import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
 @ComponentScan()
@@ -31,92 +30,124 @@ public class TestServicesAddress {
 
     @Test
     void getById() throws AddressNotFound {
-        repository.saveAddress("Teodoro Machado", 20, "Centro", "Maringá", "Paraná");
+        AddressEntity address = new AddressEntity
+                (null, "Teodoro Machado", 20, "Maringá", "Centro", "Paraná");
 
-        AddressEntity address = repository.getAddress(1);
-        assertThat(address.getIdAddress()).isEqualTo(1);
+        services.insertAddress(address);
 
-        assertThat(services.getById(1).getStreet()).isEqualTo("Teodoro Machado");
-        assertThat(services.getById(1).getNumber()).isEqualTo(20);
-        assertThat(services.getById(1).getBoroughs()).isEqualTo("Centro");
-        assertThat(services.getById(1).getCity()).isEqualTo("Maringá");
-        assertThat(services.getById(1).getState()).isEqualTo("Paraná");
+        AddressEntity addressActual = services.getById(1);
+
+        assertThat(addressActual.getStreet()).isEqualTo(address.getStreet());
+        assertThat(addressActual.getNumber()).isEqualTo(address.getNumber());
+        assertThat(addressActual.getBoroughs()).isEqualTo(address.getBoroughs());
+        assertThat(addressActual.getCity()).isEqualTo(address.getCity());
+        assertThat(addressActual.getState()).isEqualTo(address.getState());
     }
 
     @Test
     void getAllAddress() throws RegisterAddressNotFound {
-        repository.saveAddress("Teodoro Machado", 20, "Centro", "Maringá", "Paraná");
-        repository.saveAddress("Lucas Machado", 200, "Centro Sul", "Londrina", "Paraná");
-
-        assertThat(services.getAllAddress().size()).isEqualTo(2);
-        assertThat(services.getAllAddress().get(0).getIdAddress()).isEqualTo(1);
-        assertThat(services.getAllAddress().get(0).getStreet()).isEqualTo("Teodoro Machado");
-        assertThat(services.getAllAddress().get(0).getNumber()).isEqualTo(20);
-        assertThat(services.getAllAddress().get(0).getBoroughs()).isEqualTo("Centro");
-        assertThat(services.getAllAddress().get(0).getCity()).isEqualTo("Maringá");
-        assertThat(services.getAllAddress().get(0).getState()).isEqualTo("Paraná");
-    }
-
-    @Test
-    void insert() throws ErrorSavingAddress {
         AddressEntity address = new AddressEntity
                 (1, "Teodoro Machado", 20, "Maringá", "Centro", "Paraná");
-        assertThat(address.getIdAddress()).isEqualTo(1);
+        AddressEntity address2 = new AddressEntity
+                (2, "Lucas Machado", 200, "Centro Sul", "Londrina", "Paraná");
 
         services.insertAddress(address);
-        assertThat(address.getIdAddress()).isEqualTo(1);
-        assertThat(address.getStreet()).isEqualTo("Teodoro Machado");
-        assertThat(address.getNumber()).isEqualTo(20);
-        assertThat(address.getBoroughs()).isEqualTo("Centro");
-        assertThat(address.getCity()).isEqualTo("Maringá");
-        assertThat(address.getState()).isEqualTo("Paraná");
+        services.insertAddress(address2);
+
+        List<AddressEntity> list = services.getAllAddress();
+
+        assertThat(list.size()).isEqualTo(2);
+        assertThat(list.get(0).getIdAddress()).isEqualTo(address.getIdAddress());
+        assertThat(list.get(0).getStreet()).isEqualTo(address.getStreet());
+        assertThat(list.get(0).getNumber()).isEqualTo(address.getNumber());
+        assertThat(list.get(0).getBoroughs()).isEqualTo(address.getBoroughs());
+        assertThat(list.get(0).getCity()).isEqualTo(address.getCity());
+        assertThat(list.get(0).getState()).isEqualTo(address.getState());
     }
 
     @Test
-    void update() throws AddressNotFound {
+    void insert() throws  AddressNotFound {
+        AddressEntity address = new AddressEntity
+                (null, "Teodoro Machado", 20, "Maringá", "Centro", "Paraná");
 
-        repository.saveAddress("Teodoro Machado", 20, "Centro", "Maringá", "Paraná");
-        repository.saveAddress("Lucas Machado", 200, "Centro Sul", "Londrina", "Paraná");
+        services.insertAddress(address);
 
-        AddressEntity address = repository.getAddress(1);
-        AddressEntity address1 = repository.getAddress(2);
+        AddressEntity addressActual = services.getById(1);
 
-        services.updateAddress(2, address1);
-
-        assertThat(address.getIdAddress()).isEqualTo(1);
-        assertThat(address.getStreet()).isEqualTo("Teodoro Machado");
-        assertThat(address.getNumber()).isEqualTo(20);
-        assertThat(address.getBoroughs()).isEqualTo("Centro");
-        assertThat(address.getCity()).isEqualTo("Maringá");
-        assertThat(address.getState()).isEqualTo("Paraná");
-
-        assertThat(address1.getIdAddress()).isEqualTo(2);
-        assertThat(address1.getStreet()).isEqualTo("Lucas Machado");
-        assertThat(address1.getNumber()).isEqualTo(200);
-        assertThat(address1.getBoroughs()).isEqualTo("Centro Sul");
-        assertThat(address1.getCity()).isEqualTo("Londrina");
-        assertThat(address1.getState()).isEqualTo("Paraná");
+        assertThat(addressActual.getStreet()).isEqualTo(address.getStreet());
+        assertThat(addressActual.getNumber()).isEqualTo(address.getNumber());
+        assertThat(addressActual.getBoroughs()).isEqualTo(address.getBoroughs());
+        assertThat(addressActual.getCity()).isEqualTo(address.getCity());
+        assertThat(addressActual.getState()).isEqualTo(address.getState());
     }
 
     @Test
-    void delete() throws AddressNotFound {
-        repository.saveAddress("Teodoro Machado", 20, "Centro", "Maringá", "Paraná");
-        repository.saveAddress("Lucas Machado", 200, "Centro Sul", "Londrina", "Paraná");
-        List<AddressEntity> list = repository.getAllAddress();
+    void update() throws AddressNotFound, RegisterAddressNotFound {
+        AddressEntity address1 = new AddressEntity
+                (null, "Teodoro", 20, "Maringá", "Centro", "Paraná");
+        AddressEntity address2 = new AddressEntity
+                (null, "Lucas", 200, "Londrina", "Centro Sul", "Paraná");
+
+        services.insertAddress(address1);
+        assertThat(address1.getStreet()).isEqualTo("Teodoro");
+
+        services.updateAddress(1, address2);
+
+        List<AddressEntity> list = services.getAllAddress();
+        assertThat(list.size()).isEqualTo(1);
+
+        assertThat(list.get(0).getIdAddress()).isEqualTo(1);
+        assertThat(list.get(0).getStreet()).isEqualTo("Lucas");
+    }
+
+    @Test
+    void delete() throws AddressNotFound, RegisterAddressNotFound {
+        AddressEntity addressMaringa = new AddressEntity
+                (null, "Teodoro", 20, "Maringá", "Centro", "Paraná");
+        AddressEntity addressLondrina = new AddressEntity
+                (null, "Lucas", 200, "Londrina", "Centro Sul", "Paraná");
+
+        services.insertAddress(addressMaringa);
+        services.insertAddress(addressLondrina);
+
+        List<AddressEntity> list = services.getAllAddress();
         assertThat(list.size()).isEqualTo(2);
 
         services.deleteAddress(1);
-        AddressEntity address1 = repository.getAddress(2);
+        AddressEntity addressActual = services.getById(2);
 
-        List<AddressEntity> list2 = repository.getAllAddress();
+        List<AddressEntity> list2 = services.getAllAddress();
         assertThat(list2.size()).isEqualTo(1);
 
-        assertThat(address1.getIdAddress()).isEqualTo(2);
-        assertThat(address1.getStreet()).isEqualTo("Lucas Machado");
-        assertThat(address1.getNumber()).isEqualTo(200);
-        assertThat(address1.getBoroughs()).isEqualTo("Centro Sul");
-        assertThat(address1.getCity()).isEqualTo("Londrina");
-        assertThat(address1.getState()).isEqualTo("Paraná");
+        assertThat(list2.get(0).getIdAddress()).isEqualTo(addressActual.getIdAddress());
+        assertThat(list2.get(0).getStreet()).isEqualTo(addressActual.getStreet());
+        assertThat(list2.get(0).getNumber()).isEqualTo(addressActual.getNumber());
+        assertThat(list2.get(0).getBoroughs()).isEqualTo(addressActual.getBoroughs());
+        assertThat(list2.get(0).getCity()).isEqualTo(addressActual.getCity());
+        assertThat(list2.get(0).getState()).isEqualTo(addressActual.getState());
+    }
+
+    @Test
+    void exceptionAddressNotFound() {
+
+        Throwable exception = catchThrowable(() -> {
+            services.getById(2);
+        });
+
+        assertThat(exception)
+                .isInstanceOf(AddressNotFound.class)
+                .hasMessageContaining("O endereço com o id 2 não foi encontrado.");
+    }
+    @Test
+    void exceptionRegisterAddressNotFound() {
+
+        Throwable exception = catchThrowable(() -> {
+            services.getAllAddress();
+        });
+
+        assertThat(exception)
+                .isInstanceOf(RegisterAddressNotFound.class)
+                .hasMessageContaining("Nenhum endereço foi cadastrado.");
     }
 }
 
