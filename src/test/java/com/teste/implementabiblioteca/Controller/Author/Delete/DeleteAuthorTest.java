d95c63ed9a9e1884cebdb9671642247fa1198640
Bevilhonda <marcelinyuri@gmail.com>
2023-11-07 16:41:18 -0300

    cria classe de teste para controlers.DeleteAuthor

--- src/test/java/com/teste/implementabiblioteca/Controller/Author/Delete/DeleteAuthorTest.java
+++ src/test/java/com/teste/implementabiblioteca/Controller/Author/Delete/DeleteAuthorTest.java
@@ -1,26 +1,20 @@
 package com.teste.implementabiblioteca.Controller.Author.Delete;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.teste.implementabiblioteca.Model.Author.AuthorEntity;
+import com.teste.implementabiblioteca.Model.Author.Exceptions.AuthorNotFound;
 import com.teste.implementabiblioteca.Services.Author.ServicesAuthor;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.time.LocalDate;
-
-import static java.time.LocalDate.parse;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -34,12 +28,15 @@ class DeleteAuthorTest {
     private ObjectMapper objectMapper;
 
     @Test
-    void deleteAuthor() throws Exception {
-        LocalDate dateBirth = parse("1995-05-03");
-        AuthorEntity author = new AuthorEntity(1,"João","Gomes",dateBirth);
+    void deleteAuthor() throws Exception, AuthorNotFound {
 
-        mockMvc.perform(MockMvcRequestBuilders.delete("/DeleteAuthor/{id}",1))
-                .andExpect(status().isAccepted());
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/DeleteAuthor/{id}", 1))
+                .andExpect(status().isOk());
+
+        Mockito.verify(service).delete(1);
+
+        Mockito.verify(service, times(1)).delete(1);
 
     }
 }
