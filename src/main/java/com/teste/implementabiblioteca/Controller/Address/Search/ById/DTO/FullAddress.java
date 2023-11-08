e762e95cb642510e751c789a7df8d9d03c917df1
Bevilhonda <marcelinyuri@gmail.com>
2023-11-08 16:31:55 -0300

    organiza código

--- src/main/java/com/teste/implementabiblioteca/Controller/Address/Search/ById/DTO/FullAddress.java
+++ src/main/java/com/teste/implementabiblioteca/Controller/Address/Search/ById/DTO/FullAddress.java
@@ -1,9 +1,9 @@
 package com.teste.implementabiblioteca.Controller.Address.Search.ById.DTO;
 
-public record FullAddress(String street, Integer number, String neighborhood) {
+public record FullAddress(String street, Integer number, String boroughs) {
 
-    public static FullAddress createFullAddress(String street, Integer number, String neighborhood) {
-        return new FullAddress(street, number, neighborhood);
+    public static FullAddress createFullAddress(String street, Integer number, String boroughs) {
+        return new FullAddress(street, number, boroughs);
     }
 
 }
