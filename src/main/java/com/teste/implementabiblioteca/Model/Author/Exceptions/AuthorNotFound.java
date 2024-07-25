4b4055e77d9bab6d4731fc3b1e24e53090a453eb
Bevilhonda <marcelinyuri@gmail.com>
2024-07-25 09:52:55 -0300

    inclui notação @Getter

--- src/main/java/com/teste/implementabiblioteca/Model/Author/Exceptions/AuthorNotFound.java
+++ src/main/java/com/teste/implementabiblioteca/Model/Author/Exceptions/AuthorNotFound.java
@@ -1,15 +1,19 @@
 package com.teste.implementabiblioteca.Model.Author.Exceptions;
+
+import lombok.Getter;
+
 public class AuthorNotFound extends AuthorExceptions {
+
+    @Getter
     private final Integer id;
     private final String message;
+
     public AuthorNotFound(Integer id) {
         this.id = id;
         message = "O Autor com o id " + getId() + " não foi encontrado.";
 
     }
-    public Integer getId() {
-        return id;
-    }
+
     @Override
     public String getMessage() {
         return message;
